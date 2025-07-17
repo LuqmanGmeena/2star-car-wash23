f05037d2b1b0cf09eb32058c08927dddfa90e311
El Manoh <Luqmanmeena1@gmail.com>
2025-07-17 12:31:40 +0300

    Updated Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -177,7 +177,6 @@ const Home = () => {
       </section>
 
       {/* Testimonials */}
-      <section className="py-20 bg-gray-50">
       <section className="py-20 bg-white">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="text-center mb-16">
